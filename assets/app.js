3aa285154c05828bc4ed8f56bd96d78f9f8c894d
Rayan <rayan.hamdaoui@synergiefamily.com>
2025-01-18 16:08:19 +0100

    page SIO avec les débouchées SLAM/SISR et métiers possibles

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,23 @@
-import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+document.addEventListener("DOMContentLoaded", function () {
+    const sisrInfo = document.getElementById("sisrdebouche");
+    const slamInfo = document.getElementById("slamdebouche");
+    const sisrContent = document.getElementById("contentSisr");
+    const slamContent = document.getElementById("contentSlam");
 
-// View : AfficherArticles
-// Ajoute un écouteur d'événement pour intercepter la soumission du formulaire
-// document.getElementById( 'search-form' ).addEventListener( 'submit', function ( event ) {
-//     event.preventDefault(); // Empêche la soumission normale du formulaire
-//     let form = event.target; // Récupère le formulaire soumis
-//     let formData = new FormData( form ); // Crée un objet FormData avec les données du formulaire
+    if (sisrInfo && slamInfo && sisrContent && slamContent) {
+        sisrInfo.addEventListener("click", function () {
+            sisrContent.style.display = "flex";
+            sisrContent.style.flexDirection = "column";
+            sisrContent.style.alignItems = "center";
+            slamContent.style.display = "none";
+        });
 
-//     // Envoie une requête fetch avec les données du formulaire
-//     fetch( form.action, {
-//         method: form.method, // Utilise la méthode du formulaire (GET ou POST)
-//         body: formData, // Envoie les données du formulaire
-//         headers: {
-//             'X-Requested-With': 'XMLHttpRequest' // Indique que la requête est faite en AJAX
-//         }
-//     } )
-//         .then( response => response.json() ) // Convertit la réponse en JSON
-//         .then( data => {
-//             let articlesContainer = document.getElementById( 'articles-container' ); // Récupère le conteneur des articles
-//             articlesContainer.innerHTML = data.content; // Met à jour le contenu du conteneur avec les nouveaux articles
-//         } )
-//         .catch( error => console.error( 'Error:', error ) ); // Affiche une erreur en cas de problème
-// } );
+        // Gérer le clic sur le bouton SLAM
+        slamInfo.addEventListener("click", function () {
+            slamContent.style.display = "flex";
+            slamContent.style.flexDirection = "column";
+            slamContent.style.alignItems = "center";
+            sisrContent.style.display = "none";
+        });
+    }
+});
