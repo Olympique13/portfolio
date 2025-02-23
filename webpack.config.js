19f78c3eda7ae483a4fa4c07625ea0e07be01557
Olympique13 <rayan.hamdaoui@charlespeguymarseille.com>
2025-02-23 23:37:49 +0100

    animation homepage

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry( 'app', './assets/app.js' )
+    .addEntry( 'homepage', './assets/homepage.js' )
     .addEntry( 'competence', './assets/competence.js')
     .addEntry( 'contact', './assets/contact.js')
 
